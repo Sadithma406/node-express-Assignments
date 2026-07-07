cdcb4eef83610872a736fa4f031fce04d8a8b3e6
SadithmaPerera <sadithma.20242193@iit.ac.lk>
2026-07-07 19:53:30 +0530

    using mongoose schema in mongoDB database

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
+mongoose.connect(process.env.MongoDB_String)
+  .then(() => console.log("Database connection successful"))
+  .catch(err => { console.log(err) });
+const userSchema = new mongoose.Schema({
+  name: { type: String },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+})
+const User = new mongoose.model("users", userSchema);
 // create express server app
 const app = express()
 
@@ -10,12 +22,18 @@ app.use(express.static("client"))
 app.use(express.json())
 
 // define api
-app.post("/api/login", (req, res) => {
+app.post("/api/login", async (req, res) => {
   const { email, password } = req.body;
-  if (email === "name@example.com" && password === "Pas$w0rd") {
-    res.send({ success: true })
-  }
-  else {
+  try {
+    const user = await User.findOne({ email: email, password: password })
+    if (user) {
+      res.send({ success: true })
+    }
+    else {
+      res.send({ success: false })
+    }
+  } catch (err) {
+    console.log(err)
     res.send({ success: false })
   }
 })
